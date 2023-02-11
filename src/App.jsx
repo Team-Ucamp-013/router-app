ffa1beaf5e41c9c66de98f2624486739f91d287f
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2023-02-11 11:15:40 -0600

    joke chuck norris

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const pacientes = [
-    {
-      nombre: 'Fatima',
-      edad: 25,
-    },
-    {
-      nombre: 'Athziri',
-      edad: 25,
-    },
-    {
-      nombre: 'Antonio',
-      edad: 29,
-    },
-    {
-      nombre: 'Dany',
-      edad: 25,
-    },
-    {
-      nombre: 'Omar',
-      edad: 23,
-    },
-    {
-      nombre: 'Carlos',
-      edad: 30,
-    }
-  ]
+  const [joke, setJoke] = useState()
+
+  const getJoke = async () => {
+    const response = await axios('https://api.chucknorris.io/jokes/random')
+    console.log('joke', response.data)
+    setJoke(response.data)
+  }
+
+  useEffect(() => {
+    getJoke()
+  }, []);
 
   return (
     <div className="App">
       <h1>Hola mundo</h1>
       <Container>
         <Row>
-          {
-            pacientes.map((item, index) => (
-              <Col md={4}>
-                <Card key={index} style={{ width: '18rem' }}>
-                  <Card.Body>
-                    <Card.Title>{item.nombre}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">{item.edad}</Card.Subtitle>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))
-          }
+          <Col>
+            <Card>
+              <Card.Body>
+                <Card.Text>
+                  {joke.value}
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
       </Container>
     </div>
